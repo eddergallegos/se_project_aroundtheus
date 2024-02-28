889c6bce1379b8cc1419b2eb996498cf36465017
Edder <eddergallegos@yahoo.com>
2024-02-27 18:34:51 -0700

    update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -40,18 +40,34 @@ console.log(initialCards);
 
 const profileEditButton = document.querySelector("#profile-edit-button");
 const profileEditModal = document.querySelector("#profile-edit-modal");
+const profileEditCloseButton = profileEditModal.querySelector(".modal__close");
+const profileTitle = document.querySelector(".profile__title");
+const profileDescription = document.querySelector(".profile__description");
+const profileTitleInput = document.querySelector("#profile-title-input");
+const profileDescriptionInput = document.querySelector(
+  "#profile-description-input"
+);
+const profileEditForm = profileEditModal.querySelector(".modal__form");
+
+function closePopop() {
+  profileEditModal.classList.remove("modal_opened");
+}
+
+function handleProfileEditSubmit(e) {
+  e.preventDefault();
+  profileTitle.textContent = profileTitleInput.value;
+  profileDescription.textContent = profileDescriptionInput.value;
+  closePopop();
+}
 
 profileEditButton.addEventListener("click", () => {
+  profileTitleInput.value = profileTitle.textContent;
+  profileDescriptionInput.value = profileDescription.textContent;
   profileEditModal.classList.add("modal_opened");
 });
 
-console.log(profileEditButton);
-
-const modalCloseButton = document.querySelector("#modal-close-button");
-const modal = document.querySelector("#profile-edit-modal");
-
-modalCloseButton.addEventListener("click", () => {
-  modal.style.display = "none";
+profileEditCloseButton.addEventListener("click", () => {
+  closePopop();
 });
 
-console.log(modalCloseButton);
+profileEditForm.addEventListener("submit", handleProfileEditSubmit);
